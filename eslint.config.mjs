0d861a332760842cf59b97ecc268f4442b3344c0
chrisfarms <chris@farmiloe.com>
2024-07-02 07:15:07 +0100

    setup prettier/eslint better

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     eslintConfigPrettier,
-    eslintPluginPrettier,
 ];
