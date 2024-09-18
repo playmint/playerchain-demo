f8ba52d4bf59edc97cac5e825791e9dbc76e9e4a
chrisfarms <chris@farmiloe.com>
2024-09-18 14:34:07 +0100

    rm provider

--- src/gui/App.tsx
+++ src/gui/App.tsx
@@ -2,7 +2,6 @@ import { Layout } from './components/Layout';
 import { ClientProvider } from './providers/ClientProvider';
 import { CredentialsProvider } from './providers/CredentialsProvider';
 import { DatabaseProvider } from './providers/DatabaseProvider';
-import { PackerLaceProvider } from './providers/PacketLaceProvider';
 import { SettingsProvider } from './providers/SettingsProvider';
 import { SocketProvider } from './providers/SocketProvider';
 
@@ -12,11 +11,9 @@ export default function App(_props: { instance: number }) {
             <CredentialsProvider>
                 <DatabaseProvider>
                     <ClientProvider>
-                        <PackerLaceProvider>
-                            <SettingsProvider>
-                                <Layout />
-                            </SettingsProvider>
-                        </PackerLaceProvider>
+                        <SettingsProvider>
+                            <Layout />
+                        </SettingsProvider>
                     </ClientProvider>
                 </DatabaseProvider>
             </CredentialsProvider>
