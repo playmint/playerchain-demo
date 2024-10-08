9806aaf95fe824d37747fe525147969881d94225
Steedie <Steediekid@gmail.com>
2024-10-08 18:43:07 +0100

    wall components in their own files

--- src/examples/spaceshooter/renderer/WallModels.tsx
+++ src/examples/spaceshooter/renderer/WallModels.tsx
@@ -1,70 +1,9 @@
-import { Clone, useGLTF } from '@react-three/drei';
 import { memo } from 'react';
 import { Vector3 } from 'three';
-import wall_cornerMediumGLTF from '../assets/wall_cornerMedium2.glb?url';
-import wall_largeGLTF from '../assets/wall_large2.glb?url';
-import wall_mediumGLTF from '../assets/wall_medium2.glb?url';
 import levelData from '../levels/level_1';
-import { assetPath } from '../utils/RenderUtils';
-
-const WallLarge = ({
-    position,
-    rotation,
-}: {
-    position: Vector3;
-    rotation: number;
-}) => {
-    const { scene: wallLarge } = useGLTF(assetPath(wall_largeGLTF));
-    return (
-        <Clone
-            object={wallLarge}
-            position={[position.x, position.y, 0]}
-            rotation={[0, 0, rotation]}
-            scale={[10, 10, 10]}
-            deep
-        />
-    );
-};
-
-const WallMedium = ({
-    position,
-    rotation,
-}: {
-    position: Vector3;
-    rotation: number;
-}) => {
-    const { scene: wallMedium } = useGLTF(assetPath(wall_mediumGLTF));
-    return (
-        <Clone
-            object={wallMedium}
-            position={[position.x, position.y, 0]}
-            rotation={[0, 0, rotation]}
-            scale={[10, 10, 10]}
-            deep
-        />
-    );
-};
-
-const WallCornerMedium = ({
-    position,
-    rotation,
-}: {
-    position: Vector3;
-    rotation: number;
-}) => {
-    const { scene: wallCornerMedium } = useGLTF(
-        assetPath(wall_cornerMediumGLTF),
-    );
-    return (
-        <Clone
-            object={wallCornerMedium}
-            position={[position.x, position.y, 0]}
-            rotation={[0, 0, rotation]}
-            scale={[10, 10, 10]}
-            deep
-        />
-    );
-};
+import { WallCornerMedium } from './WallCornerMedium';
+import { WallLarge } from './WallLarge';
+import { WallMedium } from './WallMedium';
 
 export default memo(function WallModels() {
     const { models } = levelData;
