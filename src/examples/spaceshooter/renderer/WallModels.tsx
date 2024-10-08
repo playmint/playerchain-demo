e75ddc78bd16bac83b8c632571756c6aca6e4156
Steedie <Steediekid@gmail.com>
2024-10-08 14:16:33 +0100

    use r3f components instead of manually cloning

--- src/examples/spaceshooter/renderer/WallModels.tsx
+++ src/examples/spaceshooter/renderer/WallModels.tsx
@@ -1,5 +1,5 @@
-import { useGLTF } from '@react-three/drei';
-import { memo, useEffect, useRef } from 'react';
+import { Clone, useGLTF } from '@react-three/drei';
+import { memo } from 'react';
 import * as THREE from 'three';
 import wall_cornerMediumGLTF from '../assets/wall_cornerMedium2.glb?url';
 import wall_largeGLTF from '../assets/wall_large2.glb?url';
@@ -7,62 +7,106 @@ import wall_mediumGLTF from '../assets/wall_medium2.glb?url';
 import levelData from '../levels/level_1';
 import { assetPath } from '../utils/RenderUtils';
 
-export default memo(function WallModels() {
-    const { models } = levelData;
-
-    const { scene: wallMedium } = useGLTF(assetPath(wall_mediumGLTF));
+const WallLarge = ({
+    position,
+    rotation,
+}: {
+    position: THREE.Vector3;
+    rotation: number;
+}) => {
     const { scene: wallLarge } = useGLTF(assetPath(wall_largeGLTF));
+    return (
+        <Clone
+            object={wallLarge}
+            position={[position.x, position.y, 0]}
+            rotation={[0, 0, rotation]}
+            scale={[10, 10, 10]}
+            deep
+        />
+    );
+};
+
+const WallMedium = ({
+    position,
+    rotation,
+}: {
+    position: THREE.Vector3;
+    rotation: number;
+}) => {
+    const { scene: wallMedium } = useGLTF(assetPath(wall_mediumGLTF));
+    return (
+        <Clone
+            object={wallMedium}
+            position={[position.x, position.y, 0]}
+            rotation={[0, 0, rotation]}
+            scale={[10, 10, 10]}
+            deep
+        />
+    );
+};
+
+const WallCornerMedium = ({
+    position,
+    rotation,
+}: {
+    position: THREE.Vector3;
+    rotation: number;
+}) => {
     const { scene: wallCornerMedium } = useGLTF(
         assetPath(wall_cornerMediumGLTF),
     );
+    return (
+        <Clone
+            object={wallCornerMedium}
+            position={[position.x, position.y, 0]}
+            rotation={[0, 0, rotation]}
+            scale={[10, 10, 10]}
+            deep
+        />
+    );
+};
 
-    const groupRef = useRef<THREE.Group>(null);
+export default memo(function WallModels() {
+    const { models } = levelData;
 
-    useEffect(() => {
-        if (groupRef.current) {
-            placeModels();
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    const placeModels = () => {
-        if (!groupRef.current) {
-            return;
-        }
-
-        models.forEach((model) => {
-            let modelInstance;
-            switch (model.name) {
-                case 'wallLarge':
-                    modelInstance = wallLarge.clone();
-                    break;
-                case 'wallMedium':
-                    modelInstance = wallMedium.clone();
-                    break;
-                case 'wallCornerMedium':
-                    modelInstance = wallCornerMedium.clone();
-                    break;
-                default:
-                    console.warn(`Unknown model name: ${model.name}`);
-                    return;
-            }
-
-            if (modelInstance) {
-                modelInstance.position.set(
+    return (
+        <group>
+            {models.map((model, index) => {
+                const position = new THREE.Vector3(
                     model.position.x,
                     model.position.y,
                     0,
                 );
-                modelInstance.rotation.z = model.rotation;
-                modelInstance.scale.set(10, 10, 10);
-
-                if (groupRef.current) {
-                    groupRef.current.add(modelInstance);
+                switch (model.name) {
+                    case 'wallLarge':
+                        return (
+                            <WallLarge
+                                key={index}
+                                position={position}
+                                rotation={model.rotation}
+                            />
+                        );
+                    case 'wallMedium':
+                        return (
+                            <WallMedium
+                                key={index}
+                                position={position}
+                                rotation={model.rotation}
+                            />
+                        );
+                    case 'wallCornerMedium':
+                        return (
+                            <WallCornerMedium
+                                key={index}
+                                position={position}
+                                rotation={model.rotation}
+                            />
+                        );
+                    default:
+                        console.warn(`Unknown model name: ${model.name}`);
+                        return null;
                 }
-            }
-        });
-        console.log('Rendered wall models');
-    };
-
-    return <group ref={groupRef} />;
+            })}
+        </group>
+    );
 });
