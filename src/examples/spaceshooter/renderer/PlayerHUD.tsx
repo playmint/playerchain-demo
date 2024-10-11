7f1f061e514edee882476101507d04e643ee4a08
chrisfarms <chris@farmiloe.com>
2024-10-11 13:43:49 +0100

    clock and countin

--- src/examples/spaceshooter/renderer/PlayerHUD.tsx
+++ src/examples/spaceshooter/renderer/PlayerHUD.tsx
@@ -1,6 +1,11 @@
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
+import {
+    FIXED_UPDATE_RATE,
+    SIM_END,
+} from '../../../gui/components/ChannelView';
 import { PlayerData } from '../../../runtime/ecs';
-import { ShooterSchema } from '../../spaceshooter';
+import { SESSION_TIME_SECONDS, ShooterSchema } from '../../spaceshooter';
+import ReadySetGo from './Countdown';
 import EnergyBar from './EnergyBar';
 import LeaderBoard from './LeaderBoard';
 import { WorldRef } from './ShooterRenderer';
@@ -14,15 +19,29 @@ export type PlayerInfo = Omit<PlayerData<ShooterSchema['player']>, 'input'> & {
 export default memo(function PlayerHUD({
     peerId,
     players,
+    worldRef,
 }: {
     players: PlayerInfo[];
     peerId: string;
     worldRef: WorldRef;
 }) {
+    const [remaining, setRemaining] = useState(0);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (!worldRef.current) {
+                return;
+            }
+            const tick = worldRef.current.t;
+            const remainingTicks = Math.max(0, SIM_END - tick);
+            const remainingSeconds =
+                remainingTicks * (FIXED_UPDATE_RATE / 1000);
+            // format as MM:SS with leading zeros
+            setRemaining(remainingSeconds);
+        }, 1000);
+        return () => clearInterval(timer);
+    }, [worldRef]);
+
     const player = players.find((p) => p.id === peerId);
-    if (!player) {
-        return null;
-    }
     return (
         <div
             style={{
@@ -38,14 +57,19 @@ export default memo(function PlayerHUD({
             }}
         >
             <div></div>
-            <div style={{ flexGrow: 1 }}></div>
-            {/* <Countdown
-                currentTick={tick}
-                player={player}
-                worldRef={worldRef}
-                players={players}
-                peerId={peerId}
-            /> */}
+            <div style={{ flexGrow: 1 }}>
+                {remaining > 0 && (
+                    <div style={{ padding: 10 }}>
+                        {String(Math.floor(remaining / 60)).padStart(2, '0')}:
+                        {String(Math.ceil(remaining % 60)).padStart(2, '0')}
+                    </div>
+                )}
+            </div>
+            {SESSION_TIME_SECONDS - remaining < 3 && (
+                <ReadySetGo
+                    n={3 - Math.floor(SESSION_TIME_SECONDS - remaining)}
+                />
+            )}
             <div
                 style={{
                     display: 'flex',
@@ -63,7 +87,7 @@ export default memo(function PlayerHUD({
                         marginLeft: '1rem',
                     }}
                 >
-                    <EnergyBar energy={player.health} />
+                    {player && <EnergyBar energy={player.health} />}
                 </div>
                 <div style={{ width: '30%' }}>
                     <LeaderBoard players={players} peerId={peerId} />
