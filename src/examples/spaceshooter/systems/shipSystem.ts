c86fc1ddccdd4934231742ca0032b1158881d626
speedything <paul.woodbridge@playmint.com>
2024-10-14 11:04:39 +0100

    Faster ships, bullets despawn when killed, bullets inherit less velocity from parent

--- src/examples/spaceshooter/systems/shipSystem.ts
+++ src/examples/spaceshooter/systems/shipSystem.ts
@@ -14,7 +14,7 @@ import level from '../levels/level_1';
 export const SHIP_THRUST_RATE = 90;
 export const SHIP_ROTATION_RATE = Math.fround(Math.PI / 0.7);
 export const SHIP_RESPAWN_RADIUS = level.spawnRadius;
-export const SHIP_MAX_VELOCITY = 72;
+export const SHIP_MAX_VELOCITY = 85;
 export const BULLET_DAMAGE = 100;
 
 export default system<ShooterSchema>(
