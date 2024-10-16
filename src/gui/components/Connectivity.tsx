459e256547992d0dc3ccb2678e95a3cecfa3878f
Steedie <Steediekid@gmail.com>
2024-10-16 15:11:12 +0100

    updated connectivity warning icons and text
    better visibility & clarity

--- src/gui/components/Connectivity.tsx
+++ src/gui/components/Connectivity.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Metric } from '../../runtime/metrics';
 import theme from '../styles/default.module.css';
 
-const SHOW_AT_THRESHOLD = 5;
+const SHOW_AT_THRESHOLD = 6;
 
 export default function Connectivity({
     metric,
@@ -12,8 +12,9 @@ export default function Connectivity({
     peerCount: number;
 }) {
     const [state, setState] = useState({
-        showConnectivity: true,
+        showConnectivity: false,
         color: '#fac905',
+        icon: 'sentiment_dissatisfied',
     });
 
     useEffect(() => {
@@ -21,6 +22,10 @@ export default function Connectivity({
             setState({
                 showConnectivity: value <= SHOW_AT_THRESHOLD && peerCount > 0,
                 color: value < 1 ? '#FF0000' : '#fac905',
+                icon:
+                    value < 1
+                        ? 'sentiment_very_dissatisfied'
+                        : 'sentiment_dissatisfied',
             });
         });
     }, [metric, peerCount]);
@@ -28,17 +33,33 @@ export default function Connectivity({
     return (
         <>
             {state.showConnectivity && (
-                <span
+                <div
                     style={{
                         position: 'absolute',
                         top: '1rem',
                         left: '1rem',
+                        display: 'flex',
+                        alignItems: 'center',
                         color: state.color,
                     }}
-                    className={theme.materialSymbolsOutlined}
                 >
-                    link
-                </span>
+                    <span
+                        style={{
+                            fontSize: '2rem',
+                        }}
+                        className={theme.materialSymbolsOutlined}
+                    >
+                        {state.icon}
+                    </span>
+                    <span
+                        style={{
+                            color: 'white',
+                            marginLeft: '0.5rem',
+                        }}
+                    >
+                        connection issue
+                    </span>
+                </div>
             )}
         </>
     );
