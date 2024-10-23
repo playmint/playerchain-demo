6bd6f6b6928e2d273e744ca925c2d267aa9ecef3
Steedie <Steediekid@gmail.com>
2024-10-23 12:42:45 +0100

    implemented sigmoid curve explosion effect falloff

--- src/examples/spaceshooter/renderer/ShakeManager.tsx
+++ src/examples/spaceshooter/renderer/ShakeManager.tsx
@@ -19,10 +19,19 @@ export function getShakeOffset(
     deltaTime: number,
 ): Vector3 {
     const shakeOffset = new Vector3(0, 0, 0);
+    const useCameraPosition: Vector3 = cameraPosition.clone();
+    useCameraPosition.z = 0;
 
     activeShakes = activeShakes.filter((shake) => {
-        const distance = cameraPosition.distanceTo(shake.position);
-        const attenuation = 1 / (distance + 1);
+        const distance = useCameraPosition.distanceTo(shake.position);
+
+        // Sigmoid function for falloff
+        const distanceThreshold = 75; // Distance at which falloff starts
+        const curveSteepness = 15;
+        const maxAttenuation = 0.01;
+        const attenuation =
+            maxAttenuation /
+            (1 + Math.exp((distance - distanceThreshold) / curveSteepness));
 
         const shakeIntensity = shake.intensity * attenuation;
         shakeOffset.x +=
