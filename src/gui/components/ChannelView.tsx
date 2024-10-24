1154270eb43832c6f7beacd2af1155075652e879
Chris Farmiloe <chris@farmiloe.com>
2024-10-24 08:15:12 +0100

    Revert "setting peer version from keep alive"
    This reverts commit 916bbb49aeae8e2b11079f9ca3700b4d07be3dfb.

--- src/gui/components/ChannelView.tsx
+++ src/gui/components/ChannelView.tsx
@@ -134,14 +134,6 @@ export default memo(function ChannelView({
         [],
     );
 
-    const peerVersions = useLiveQuery(
-        () => {
-            return db.peerVesions.toArray();
-        },
-        [],
-        [],
-    );
-
     // a peer is "ready" if it can see all other peers
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const required = channel
@@ -307,17 +299,7 @@ export default memo(function ChannelView({
                         {peerNames.find((p) => p.peerId === channel.creator)
                             ?.name || channel.creator.slice(0, 8)}
                     </span>{' '}
-                    to confirm peers.
-                    <br />
-                    Using version{' '}
-                    <span style={{ color: 'white' }}>
-                        v
-                        {
-                            peerVersions.find(
-                                (p) => p.peerId === channel.creator,
-                            )?.version
-                        }
-                    </span>{' '}
+                    to confirm peers
                 </span>
             ),
             promise: () =>
--- src/runtime/db.ts
+++ src/runtime/db.ts
@@ -74,14 +74,9 @@ export type PlayerSettings = {
     muted?: boolean;
 };
 
-export type PeerName = {
+export type PeerNames = {
     peerId: string;
     name: string;
-};
-
-export type PeerVersions = {
-    peerId: string;
-    version: string;
 };
 
 export type MessageConfirmationMatrix = [
@@ -231,8 +226,7 @@ export type DB = Dexie & {
     peers: EntityTable<PeerInfo, 'peerId'>;
     network: EntityTable<NetworkInfo, 'id'>;
     settings: EntityTable<PlayerSettings, 'id'>;
-    peerNames: EntityTable<PeerName, 'peerId'>;
-    peerVesions: EntityTable<PeerVersions, 'peerId'>;
+    peerNames: EntityTable<PeerNames, 'peerId'>;
     tapes: EntityTable<Tape>;
     chat: EntityTable<StoredChatMessage, 'id'>;
 };
@@ -249,7 +243,6 @@ export function open(name: string): DB {
         network: 'id',
         settings: 'id',
         peerNames: 'peerId',
-        peerVesions: 'peerId',
         tapes: '[channel+round], [channel+updated]',
         chat: 'id, arrived',
     });
