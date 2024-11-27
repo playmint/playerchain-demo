3b01494d688d404eba4918ed359c6f8bfe1ed3d9
chrisfarms <chris@farmiloe.com>
2024-11-27 11:11:59 +0000

    remove sea boostrap nodes

--- src/runtime/bootstrap.ts
+++ src/runtime/bootstrap.ts
@@ -1,5 +1,4 @@
 const northEurope = '51.158.36.190';
-const southEastAsia = '20.191.154.190';
 
 // well known playmint bootstrap peers for when you haven't got any friends
 export const BOOTSTRAP_PEERS = [
@@ -31,34 +30,6 @@ export const BOOTSTRAP_PEERS = [
         natType: 31,
         indexed: true,
     },
-    {
-        address: southEastAsia,
-        port: 47080,
-        peerId: 'b842b2a3f8e4a5d9871c0cc5d95a87207aec3a1ee242dd89c4eead0b39ecd2c8',
-        natType: 31,
-        indexed: true,
-    },
-    {
-        address: southEastAsia,
-        port: 47081,
-        peerId: '4fa4c40d334055a086785572532a110880503bf410c6bc254dde298bd24626f8',
-        natType: 31,
-        indexed: true,
-    },
-    {
-        address: southEastAsia,
-        port: 47082,
-        peerId: '17c450ec4e53a895819bfbe997bd3bfb0076c51269cda0e290e95f100a839d3a',
-        natType: 31,
-        indexed: true,
-    },
-    {
-        address: southEastAsia,
-        port: 47083,
-        peerId: 'ee590d62d17bb08c98554218609671dfae5341b4dea8f84a190198bda4df61c8',
-        natType: 31,
-        indexed: true,
-    },
 ].sort(() => Math.random() - 0.5);
 
 // debug helper for checking if one of the boostrap peers is proxying for us
@@ -67,8 +38,6 @@ export function getProxyName(peerId: string): string {
         if (peer.peerId === peerId) {
             if (peer.address === northEurope) {
                 return 'NorthEurope';
-            } else if (peer.address === southEastAsia) {
-                return 'SouthEastAsia';
             }
         }
     }
